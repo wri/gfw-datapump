15095b64480f33f75710d3bcd329a83c54ff5917
jterry64 <justin.terry@wri.org>
2019-11-22 13:38:50 -0500

    Added policy/role definitions to terraform and various fixes to code

--- lambdas/submit_job/function.py
+++ lambdas/submit_job/function.py
@@ -2,6 +2,8 @@ from botocore.exceptions import ClientError
 from geotrellis_summary_update.slack import slack_webhook
 from geotrellis_summary_update.emr import get_summary_analysis_step, submit_summary_batch_job
 import datetime
+import logging
+import traceback
 
 RESULT_BUCKET = "gfw-pipelines-dev"
 RESULT_PREFIX = "geotrellis/results/{date_version}/{name}"
@@ -23,9 +25,9 @@ def handler(event, context):
 
     try:
         steps = []
-        for analysis in analyses:
+        for analysis in analyses.keys():
             result_url = RESULT_PATH.format(RESULT_BUCKET, result_dir)
-            steps.append(get_summary_analysis_step(analysis["name"], feature_src, result_url, feature_type))
+            steps.append(get_summary_analysis_step(analysis, feature_src, result_url, feature_type))
 
         job_flow_id = submit_summary_batch_job(name, steps, "r4.xlarge", 1, env)
 
@@ -38,25 +40,27 @@ def handler(event, context):
             "feature_src": feature_src,
             "feature_type": feature_type,
             "result_bucket": RESULT_BUCKET,
-            "result_dir": result_dir
+            "result_dir": result_dir,
+            "upload_type": event["upload_type"]
         }
     except ClientError:
-        slack_webhook("ERROR", "Error submitting job to update {} summary datasets.".format(name))
+        logging.error(traceback.print_exc())
+        slack_webhook("ERROR", "Error submitting job to update {} summary datasets.".format(name), env)
         return {"status": "FAILED"}
 
 
 if __name__ == "__main__":
     print(handler({
         "env": "dev",
-        "name": "new_area_test",
+        "name": "new_area_test2",
         "feature_src": "s3://gfw-pipelines-dev/geotrellis/features/*.tsv",
         "feature_type": "geostore",
-        "analyses": [{
-            "name": "gladalerts",
-            "sub_analyses": [{
-                "name": "daily_alerts",
-                "dataset": "sdfgdsg"
-
-            }]
-        }]
+        "upload_type": "data-overwrite",
+        "analyses": {
+            "gladalerts": {
+                "daily_alerts": "72af8802-df3c-42ab-a369-5e7f2b34ae2f",
+                #"weekly_alerts": "Glad Alerts - Weekly - Geostore - User Areas",
+                #"summary": "Glad Alerts - Summary - Geostore - User Areas",
+            }
+        }
     }, None))
--- terraform/main.tf
+++ terraform/main.tf
@@ -6,11 +6,107 @@ terraform {
   }
 }
 
-resource "aws_lambda_function" "submit_batch_job" {
-  function_name    = "submit-batch-job_geotrellis-summary-update"
+
+resource "aws_iam_policy" "geotrellis_summary_update" {
+  name = "geotrellis_summary_update"
+  path = "/"
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+      {
+          "Effect": "Allow",
+          "Action": [
+              "secretsmanager:GetRandomPassword",
+              "lambda:InvokeFunction",
+              "secretsmanager:GetResourcePolicy",
+              "secretsmanager:GetSecretValue",
+              "secretsmanager:DescribeSecret",
+              "secretsmanager:ListSecretVersionIds",
+              "elasticmapreduce:RunJobFlow",
+              "elasticmapreduce:DescribeCluster",
+              "cloudformation:DescribeChangeSet",
+              "cloudformation:DescribeStackResources",
+              "cloudformation:DescribeStacks",
+              "cloudformation:GetTemplate",
+              "cloudformation:ListStackResources",
+              "cloudwatch:*",
+              "ec2:DescribeSecurityGroups",
+              "ec2:DescribeSubnets",
+              "ec2:DescribeVpcs",
+              "events:*",
+              "iam:GetPolicy",
+              "iam:GetPolicyVersion",
+              "iam:GetRole",
+              "iam:GetRolePolicy",
+              "iam:ListAttachedRolePolicies",
+              "iam:ListRolePolicies",
+              "iam:ListRoles",
+              "iam:PassRole",
+              "lambda:*",
+              "logs:*",
+              "s3:*"
+          ],
+          "Resource": "*"
+      }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role" "geotrellis_summary_update_states" {
+  name = "geotrellis_summary_update_states"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement":
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "states.us-east-1.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+}
+EOF
+}
+
+resource "aws_iam_role" "geotrellis_summary_update_lambda" {
+  name = "geotrellis_summary_update_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement":
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "geotrellis_summary_update_lambda" {
+    role       = "${aws_iam_role.geotrellis_summary_update_lambda.name}"
+    policy_arn = "${aws_iam_policy.geotrellis_summary_update.arn}"
+}
+
+resource "aws_iam_role_policy_attachment" "geotrellis_summary_update_states" {
+    role       = "${aws_iam_role.geotrellis_summary_update_states.name}"
+    policy_arn = "${aws_iam_policy.geotrellis_summary_update.arn}"
+}
+
+resource "aws_lambda_function" "submit_job" {
+  function_name    = "submit-job_geotrellis-summary-update"
   filename         = "../lambdas/submit_job/function.zip"
   source_code_hash = "${filebase64sha256("../lambdas/submit_job/function.zip")}"
-  role             = "arn:aws:iam::563860007740:role/service-role/raster-analysis-role-7aosf9ch"
+  role             = "${aws_iam_role.geotrellis_summary_update_lambda.arn}"
   runtime          = "python3.7"
   handler          = "function.handler"
   memory_size      = 128
@@ -22,7 +118,7 @@ resource "aws_lambda_function" "upload_results_to_datasets" {
   function_name    = "upload-results-to-datasets_geotrellis-summary-update"
   filename         = "../lambdas/upload_results_to_datasets/function.zip"
   source_code_hash = "${filebase64sha256("../lambdas/upload_results_to_datasets/function.zip")}"
-  role             = "arn:aws:iam::563860007740:role/service-role/raster-analysis-role-7aosf9ch"
+  role             = "${aws_iam_role.geotrellis_summary_update_lambda.arn}"
   runtime          = "python3.7"
   handler          = "function.handler"
   memory_size      = 128
@@ -34,7 +130,7 @@ resource "aws_lambda_function" "check_datasets_saved" {
   function_name    = "check-datasets-saved_geotrellis-summary-update"
   filename         = "../lambdas/check_datasets_saved/function.zip"
   source_code_hash = "${filebase64sha256("../lambdas/check_datasets_saved/function.zip")}"
-  role             = "arn:aws:iam::563860007740:role/service-role/raster-analysis-role-7aosf9ch"
+  role             = "${aws_iam_role.geotrellis_summary_update_lambda.arn}"
   runtime          = "python3.7"
   handler          = "function.handler"
   memory_size      = 128
@@ -44,7 +140,7 @@ resource "aws_lambda_function" "check_datasets_saved" {
 
 resource "aws_sfn_state_machine" "geotrellis_summary_update" {
   name     = "geotrellis-summary-update"
-  role_arn = "arn:aws:iam::563860007740:role/service-role/StepFunctionExcecution"
+  role_arn = "${aws_iam_role.geotrellis_summary_update_states.arn}"
 
   definition = "${file("../step_functions/geotrellis_summary_update.json")}"
 }
