de7ea36e96cd31c265988d80727ee20ce02bb471
Thomas Maschler <tmaschler@wri.org>
2019-12-04 15:09:47 -0500

    fix payload for geostores, catch KeyValue errors, raise meaningful errors for unexpected API responses, log actions

--- geotrellis_summary_update/secrets.py
+++ geotrellis_summary_update/secrets.py
@@ -1,3 +1,5 @@
+import json
+
 import boto3
 
 from geotrellis_summary_update.util import secret_suffix
@@ -5,4 +7,6 @@ from geotrellis_summary_update.util import secret_suffix
 
 def get_token() -> str:
     sm_client = boto3.client("secretsmanager")
-    return sm_client.get_secret_value(SecretId=f"gfw-api/{secret_suffix()}-token")
+    response = sm_client.get_secret_value(SecretId=f"gfw-api/{secret_suffix()}-token")
+
+    return json.loads(response["SecretString"])["token"]
