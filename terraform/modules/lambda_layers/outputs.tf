3a6a39745d68df715da9954e1e66718021ae92fb
Thomas Maschler <tmaschler@wri.org>
2019-12-11 14:57:12 -0500

    change layer_arn to arn

--- terraform/modules/lambda_layers/outputs.tf
+++ terraform/modules/lambda_layers/outputs.tf
@@ -1,4 +1,4 @@
 output "datapump_utils_arn" {
-  value       = aws_lambda_layer_version.datapump_utils.layer_arn
+  value       = aws_lambda_layer_version.datapump_utils.arn
   description = "ARN of datapump_utils lambda layer"
 }
