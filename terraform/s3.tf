89c3665877e6f381a69574fa112a36af519b2c80
jterry64 <justin.terry@wri.org>
2020-02-10 17:37:23 -0500

    Change extent_1x1 to geojson, not json

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,6 +1,6 @@
 resource "aws_s3_bucket_object" "extent_1x1" {
   bucket = data.terraform_remote_state.core.outputs.pipelines_bucket
-  key    = "geotrellis/features/extent_1x1.json"
-  source = "files/extent_1x1.json"
-  etag   = filemd5("../docker/datapump_utils/layer.zip")
+  key    = "geotrellis/features/extent_1x1.geojson"
+  source = "files/extent_1x1.geojson"
+  etag   = filemd5("files/extent_1x1.geojson")
 }
