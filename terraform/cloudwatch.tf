0f2897cf840a9562d5a144f8584f6719896e0126
jterry64 <justin.terry@wri.org>
2020-03-25 22:32:54 -0400

    Temporarily disable new user areas

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -11,13 +11,13 @@ resource "aws_cloudwatch_event_rule" "everyday-3-am-est" {
 }
 
 //We only want to schedule this event in production
-resource "aws_cloudwatch_event_target" "nightly-new-area-check" {
-  rule      = aws_cloudwatch_event_rule.everyday-11-pm-est.name
-  target_id = substr("${local.project}-nightly-new-area-check${local.name_suffix}", 0, 64)
-  arn       = aws_sfn_state_machine.new_user_aoi.id
-  role_arn  = aws_iam_role.datapump_states.arn
-  count     = var.environment == "production" || var.environment == "staging" ? 1 : 0
-}
+//resource "aws_cloudwatch_event_target" "nightly-new-area-check" {
+//  rule      = aws_cloudwatch_event_rule.everyday-11-pm-est.name
+//  target_id = substr("${local.project}-nightly-new-area-check${local.name_suffix}", 0, 64)
+//  arn       = aws_sfn_state_machine.new_user_aoi.id
+//  role_arn  = aws_iam_role.datapump_states.arn
+//  count     = var.environment == "production" || var.environment == "staging" ? 1 : 0
+//}
 
 //We only want to schedule this event in production
 resource "aws_cloudwatch_event_target" "nightly-new-glad-alerts-check" {
