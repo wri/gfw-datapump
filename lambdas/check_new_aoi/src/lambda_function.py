7a04da3bef8837e07e0744e2977e9fc6626cc6b7
jterry64 <justin.terry@wri.org>
2020-04-07 20:30:48 -0400

    temporarily disable geostore fire alerts

--- lambdas/check_new_aoi/src/lambda_function.py
+++ lambdas/check_new_aoi/src/lambda_function.py
@@ -81,16 +81,16 @@ def handler(event: Dict[str, Any], context: Dict[str, Any]) -> Dict[str, Any]:
                     "name": SUMMARIZE_NEW_AOIS_NAME,
                     "upload_type": "append",
                     "get_summary": True,
-                    "fire_config": {
-                        "viirs": [
-                            "s3://gfw-data-lake-dev/nasa_viirs_fire_alerts/v1/vector/epsg-4326/tsv/near_real_time/*.tsv",
-                            "s3://gfw-data-lake-dev/nasa_viirs_fire_alerts/v1/vector/epsg-4326/tsv/scientific/*.tsv",
-                        ],
-                        "modis": [
-                            "s3://gfw-data-lake-dev/nasa_modis_fire_alerts/v6/vector/epsg-4326/tsv/near_real_time/*.tsv",
-                            "s3://gfw-data-lake-dev/nasa_modis_fire_alerts/v6/vector/epsg-4326/tsv/scientific/*.tsv",
-                        ],
-                    },
+                    # "fire_config": {
+                    #     "viirs": [
+                    #         "s3://gfw-data-lake-dev/nasa_viirs_fire_alerts/v1/vector/epsg-4326/tsv/near_real_time/*.tsv",
+                    #         "s3://gfw-data-lake-dev/nasa_viirs_fire_alerts/v1/vector/epsg-4326/tsv/scientific/*.tsv",
+                    #     ],
+                    #     "modis": [
+                    #         "s3://gfw-data-lake-dev/nasa_modis_fire_alerts/v6/vector/epsg-4326/tsv/near_real_time/*.tsv",
+                    #         "s3://gfw-data-lake-dev/nasa_modis_fire_alerts/v6/vector/epsg-4326/tsv/scientific/*.tsv",
+                    #     ],
+                    # },
                 }
             else:
                 slack_webhook("INFO", "No new user areas found. Doing nothing.")
