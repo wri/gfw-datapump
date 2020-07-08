d797641b795ed2add58d9c4da5d56cc553bd227d
jterry64 <justin.terry@wri.org>
2020-07-08 17:17:09 -0400

    Fix URI for data API

--- terraform/lambdas.tf
+++ terraform/lambdas.tf
@@ -168,7 +168,6 @@ resource "aws_lambda_function" "inject_fires_data" {
   environment {
     variables = {
       ENV                = var.environment
-      DATA_API_URI       = data.terraform_remote_state.gfw-data-api.outputs.loadbalancer_dns
       DATA_API_VIIRS_VERSION = var.data_api_viirs_version
       S3_BUCKET_PIPELINE             = data.terraform_remote_state.core.outputs.pipelines_bucket
       PUBLIC_SUBNET_IDS              = jsonencode(data.terraform_remote_state.core.outputs.public_subnet_ids)
