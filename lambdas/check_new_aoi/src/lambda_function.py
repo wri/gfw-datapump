09b249c947a590855dbe92591bcf2d28adcbb9e8
jterry64 <justin.terry@wri.org>
2020-08-17 10:11:29 -0400

    Use fire alerts from production, not dev, for new areas

--- lambdas/check_new_aoi/src/lambda_function.py
+++ lambdas/check_new_aoi/src/lambda_function.py
@@ -83,12 +83,12 @@ def handler(event: Dict[str, Any], context: Dict[str, Any]) -> Dict[str, Any]:
                     "get_summary": True,
                     "fire_config": {
                         "viirs": [
-                            "s3://gfw-data-lake-dev/nasa_viirs_fire_alerts/v1/vector/epsg-4326/tsv/near_real_time/*.tsv",
-                            "s3://gfw-data-lake-dev/nasa_viirs_fire_alerts/v1/vector/epsg-4326/tsv/scientific/*.tsv",
+                            "s3://gfw-data-lake/nasa_viirs_fire_alerts/v1/vector/epsg-4326/tsv/near_real_time/*.tsv",
+                            "s3://gfw-data-lake/nasa_viirs_fire_alerts/v1/vector/epsg-4326/tsv/scientific/*.tsv",
                         ],
                         "modis": [
-                            "s3://gfw-data-lake-dev/nasa_modis_fire_alerts/v6/vector/epsg-4326/tsv/near_real_time/*.tsv",
-                            "s3://gfw-data-lake-dev/nasa_modis_fire_alerts/v6/vector/epsg-4326/tsv/scientific/*.tsv",
+                            "s3://gfw-data-lake/nasa_modis_fire_alerts/v6/vector/epsg-4326/tsv/near_real_time/*.tsv",
+                            "s3://gfw-data-lake/nasa_modis_fire_alerts/v6/vector/epsg-4326/tsv/scientific/*.tsv",
                         ],
                     },
                 }
