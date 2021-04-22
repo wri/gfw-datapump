b7c6e17a6737372baa74f491500ec3159649ade0
justin.terry@wri.org <jt@c02xrkdcjg5h.myfiosgateway.com>
2021-04-22 16:12:10 -0400

    Add better tagging for datasets

--- terraform/modules/datapump/dynamodb.tf
+++ terraform/modules/datapump/dynamodb.tf
@@ -2,6 +2,7 @@ resource "aws_dynamodb_table" "datapump" {
   name           = substr("${local.project}-datapump${local.name_suffix}", 0, 64)
   billing_mode   = "PAY_PER_REQUEST"
   hash_key       = "id"
+  tags           = local.tags
 
   attribute {
     name = "id"
