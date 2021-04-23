03951ebfe5c65fcc6a69cd5476863fce71158ed3
justin.terry@wri.org <jt@c02xrkdcjg5h.myfiosgateway.com>
2021-04-23 14:24:36 -0400

    Add tags to cloudwatch rules

--- terraform/modules/datapump/cloudwatch.tf
+++ terraform/modules/datapump/cloudwatch.tf
@@ -2,19 +2,21 @@ resource "aws_cloudwatch_event_rule" "everyday-11-pm-est" {
   name                = substr("everyday-11-pm-est${local.name_suffix}", 0, 64)
   description         = "Run everyday at 11 pm EST"
   schedule_expression = "cron(0 4 ? * * *)" // -5 to EST
+  tags                = local.tags
 }
 
 resource "aws_cloudwatch_event_rule" "everyday-9-pm-est" {
   name                = substr("everyday-9-pm-est${local.name_suffix}", 0, 64)
   description         = "Run everyday at 9 pm EST"
   schedule_expression = "cron(0 2 ? * * *)" // -5 to EST
+  tags                = local.tags
 }
 
 resource "aws_cloudwatch_event_target" "nightly-sync-areas" {
   rule      = aws_cloudwatch_event_rule.everyday-9-pm-est.name
   target_id = substr("${local.project}-nightly-sync-areas${local.name_suffix}", 0, 64)
   arn       = aws_sfn_state_machine.datapump.id
-  input    = "{\"command\": \"sync\", \"parameters\": {\"types\": [\"rw_areas\"]}}"
+  input     = "{\"command\": \"sync\", \"parameters\": {\"types\": [\"rw_areas\"]}}"
   role_arn  = aws_iam_role.datapump_states.arn
 }
 
