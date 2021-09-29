37179bec08ab5fbb87c9fe79bec4c777e80b4972
Justin Terry <justin.terry@wri.org>
2021-09-29 15:31:45 -0700

    Move integrated alerts sync time to last

--- terraform/modules/datapump/cloudwatch.tf
+++ terraform/modules/datapump/cloudwatch.tf
@@ -12,6 +12,13 @@ resource "aws_cloudwatch_event_rule" "everyday-9-pm-est" {
   tags                = local.tags
 }
 
+resource "aws_cloudwatch_event_rule" "everyday-1-am-est" {
+  name                = substr("everyday-1-am-est${local.name_suffix}", 0, 64)
+  description         = "Run everyday at 1 am EST"
+  schedule_expression = "cron(0 9 ? * * *)" // -5 to EST
+  tags                = local.tags
+}
+
 resource "aws_cloudwatch_event_target" "nightly-sync-areas" {
   rule      = aws_cloudwatch_event_rule.everyday-9-pm-est.name
   target_id = substr("${local.project}-nightly-sync-areas${local.name_suffix}", 0, 64)
@@ -25,7 +32,16 @@ resource "aws_cloudwatch_event_target" "nightly-sync" {
   rule      = aws_cloudwatch_event_rule.everyday-11-pm-est.name
   target_id = substr("${local.project}-nightly-sync${local.name_suffix}", 0, 64)
   arn       = aws_sfn_state_machine.datapump.id
-  input    = "{\"command\": \"sync\", \"parameters\": {\"types\": [\"glad\", \"integrated_alerts\", \"viirs\", \"modis\"]}}"
+  input    = "{\"command\": \"sync\", \"parameters\": {\"types\": [\"glad\", \"viirs\", \"modis\"]}}"
+  role_arn  = aws_iam_role.datapump_states.arn
+  count     = var.environment == "production" ? 1 : 0
+}
+
+resource "aws_cloudwatch_event_target" "nightly-sync-integrated" {
+  rule      = aws_cloudwatch_event_rule.everyday-1-am-est.name
+  target_id = substr("${local.project}-nightly-sync${local.name_suffix}", 0, 64)
+  arn       = aws_sfn_state_machine.datapump.id
+  input    = "{\"command\": \"sync\", \"parameters\": {\"types\": [\"integrated_alerts\"]}}"
   role_arn  = aws_iam_role.datapump_states.arn
   count     = var.environment == "production" ? 1 : 0
 }
